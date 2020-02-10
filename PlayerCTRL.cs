08b2c237ee3c530e0572ee84bb3616d8920e8984
thanonup <60838971+thanonup@users.noreply.github.com>
2020-02-10 23:49:35 +0700

    Add files via upload

---
+++ PlayerCTRL.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCTRL : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float gravity;
+    [SerializeField] float jumpSpeed;
+    [SerializeField] float rotateSpeed;
+
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+    private int jumps;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                moveDirection.y = jumpSpeed;               
+            }
+            jumps = 0;
+        }
+        else
+        {
+            moveDirection = new Vector3(0, moveDirection.y, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection.x *= speed;
+            moveDirection.z *= speed;
+            if (Input.GetKeyDown(KeyCode.Space)&& jumps < 1)
+            {
+                moveDirection.y = jumpSpeed;
+                jumps++;
+            }
+        }
+
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
